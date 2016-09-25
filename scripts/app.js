07fb2b8d4a85275dbffd5abbfb846ed4cf0681b0
hmugasha <hmug@hotmail.com>
2016-09-25 04:24:24 +0000

    initial commit

--- scripts/app.js
+++ scripts/app.js
@@ -7,11 +7,24 @@
   LunchCheckController.$inject = ["$scope"];
   function LunchCheckController($scope)
   {
-      $scope.LunchMenu = "Turkey,Olives,Cheese,Red Peppers,Beans";
-      $scope.LunchMenuCheckResult = "";
+      $scope.lunchMenu = "";
+      $scope.lunchMenuCheckResult = "";
       $scope.CheckLunchMenu = function() {
-      var LunchMenu = $scope.LunchMenu;
-        console.log( "Lunch Menu:" + LunchMenu);
+        var numberOfItems = 0;
+        var lunchMenu = $scope.lunchMenu;
+        var lunchItems = lunchMenu.split(",");
+        for(i=0; i<lunchItems.length; i++) {
+          if(lunchItems[i].trim().length > 0) {
+            numberOfItems++;
+          }
+        }
+        if(numberOfItems > 3) {
+          $scope.lunchMenuCheckResult = "Too Much!";
+        } else if(numberOfItems > 0) {
+          $scope.lunchMenuCheckResult = "Enjoy!";
+        } else {
+          $scope.lunchMenuCheckResult = "";
+        }
       };
   };
 })();
